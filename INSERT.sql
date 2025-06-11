eb7c16bd2f69a2785ac5033b327769d8a491b089
davidvedu <david.velasco.duran.alu@iesfernandoaguilar.es>
2025-06-11 10:18:16 +0200

    Commit final

---
+++ INSERT.sql
@@ -0,0 +1,191 @@
+-- Inserción de Marcas
+INSERT INTO Marcas (nombre, pais, createdAt, updatedAt) VALUES 
+('Toyota', 'Japón', NOW(), NOW()),
+('BMW', 'Alemania', NOW(), NOW()),
+('Mercedes', 'Alemania', NOW(), NOW()),
+('Audi', 'Alemania', NOW(), NOW()),
+('Volkswagen', 'Alemania', NOW(), NOW()),
+('Ford', 'Estados Unidos', NOW(), NOW()),
+('Chevrolet', 'Estados Unidos', NOW(), NOW()),
+('Honda', 'Japón', NOW(), NOW()),
+('Nissan', 'Japón', NOW(), NOW()),
+('Hyundai', 'Corea del Sur', NOW(), NOW()),
+('Kia', 'Corea del Sur', NOW(), NOW()),
+('Renault', 'Francia', NOW(), NOW()),
+('Peugeot', 'Francia', NOW(), NOW()),
+('Citroën', 'Francia', NOW(), NOW()),
+('Seat', 'España', NOW(), NOW()),
+('Fiat', 'Italia', NOW(), NOW()),
+('Mazda', 'Japón', NOW(), NOW()),
+('Opel', 'Alemania', NOW(), NOW()),
+('Volvo', 'Suecia', NOW(), NOW()),
+('Porsche', 'Alemania', NOW(), NOW()),
+('Skoda', 'Chequia', NOW(), NOW()),
+('Subaru', 'Japón', NOW(), NOW()),
+('Lexus', 'Japón', NOW(), NOW()),
+('Tesla', 'Estados Unidos', NOW(), NOW()),
+('Jeep', 'Estados Unidos', NOW(), NOW()),
+('Dacia', 'Rumanía', NOW(), NOW()),
+('Mitsubishi', 'Japón', NOW(), NOW()),
+('Alfa Romeo', 'Italia', NOW(), NOW()),
+('Suzuki', 'Japón', NOW(), NOW()),
+('Mini', 'Reino Unido', NOW(), NOW());
+
+INSERT INTO Modelo (nombre, marca_id, createdAt, updatedAt) VALUES 
+-- Toyota (1)
+('Corolla', 1, NOW(), NOW()),
+('Camry', 1, NOW(), NOW()),
+('RAV4', 1, NOW(), NOW()),
+
+-- BMW (2)
+('Serie 3', 2, NOW(), NOW()),
+('Serie 5', 2, NOW(), NOW()),
+('X5', 2, NOW(), NOW()),
+
+-- Mercedes (3)
+('Clase A', 3, NOW(), NOW()),
+('Clase C', 3, NOW(), NOW()),
+('GLC', 3, NOW(), NOW()),
+
+-- Audi (4)
+('A3', 4, NOW(), NOW()),
+('A4', 4, NOW(), NOW()),
+('Q5', 4, NOW(), NOW()),
+
+-- Volkswagen (5)
+('Golf', 5, NOW(), NOW()),
+('Polo', 5, NOW(), NOW()),
+('Tiguan', 5, NOW(), NOW()),
+
+-- Ford (6)
+('Fiesta', 6, NOW(), NOW()),
+('Focus', 6, NOW(), NOW()),
+('Mustang', 6, NOW(), NOW()),
+
+-- Chevrolet (7)
+('Spark', 7, NOW(), NOW()),
+('Cruze', 7, NOW(), NOW()),
+('Camaro', 7, NOW(), NOW()),
+
+-- Honda (8)
+('Civic', 8, NOW(), NOW()),
+('Accord', 8, NOW(), NOW()),
+('CR-V', 8, NOW(), NOW()),
+
+-- Nissan (9)
+('Micra', 9, NOW(), NOW()),
+('Qashqai', 9, NOW(), NOW()),
+('X-Trail', 9, NOW(), NOW()),
+
+-- Hyundai (10)
+('i20', 10, NOW(), NOW()),
+('Tucson', 10, NOW(), NOW()),
+('Santa Fe', 10, NOW(), NOW()),
+
+-- Kia (11)
+('Rio', 11, NOW(), NOW()),
+('Ceed', 11, NOW(), NOW()),
+('Sportage', 11, NOW(), NOW()),
+
+-- Renault (12)
+('Clio', 12, NOW(), NOW()),
+('Megane', 12, NOW(), NOW()),
+('Captur', 12, NOW(), NOW()),
+
+-- Peugeot (13)
+('208', 13, NOW(), NOW()),
+('308', 13, NOW(), NOW()),
+('3008', 13, NOW(), NOW()),
+
+-- Citroën (14)
+('C3', 14, NOW(), NOW()),
+('C4', 14, NOW(), NOW()),
+('C5 Aircross', 14, NOW(), NOW()),
+
+-- Seat (15)
+('Ibiza', 15, NOW(), NOW()),
+('Len', 15, NOW(), NOW()),
+('Arona', 15, NOW(), NOW()),
+
+-- Fiat (16)
+('500', 16, NOW(), NOW()),
+('Panda', 16, NOW(), NOW()),
+('Tipo', 16, NOW(), NOW()),
+
+-- Mazda (17)
+('Mazda3', 17, NOW(), NOW()),
+('CX-5', 17, NOW(), NOW()),
+('MX-5', 17, NOW(), NOW()),
+
+-- Opel (18)
+('Corsa', 18, NOW(), NOW()),
+('Astra', 18, NOW(), NOW()),
+('Mokka', 18, NOW(), NOW()),
+
+-- Volvo (19)
+('XC40', 19, NOW(), NOW()),
+('XC60', 19, NOW(), NOW()),
+('S60', 19, NOW(), NOW()),
+
+-- Porsche (20)
+('911', 20, NOW(), NOW()),
+('Cayenne', 20, NOW(), NOW()),
+('Macan', 20, NOW(), NOW()),
+
+-- Skoda (21)
+('Fabia', 21, NOW(), NOW()),
+('Octavia', 21, NOW(), NOW()),
+('Superb', 21, NOW(), NOW()),
+
+-- Subaru (22)
+('Impreza', 22, NOW(), NOW()),
+('Forester', 22, NOW(), NOW()),
+('Outback', 22, NOW(), NOW()),
+
+-- Lexus (23)
+('IS', 23, NOW(), NOW()),
+('NX', 23, NOW(), NOW()),
+('RX', 23, NOW(), NOW()),
+
+-- Tesla (24)
+('Model 3', 24, NOW(), NOW()),
+('Model S', 24, NOW(), NOW()),
+('Model Y', 24, NOW(), NOW()),
+
+-- Jeep (25)
+('Renegade', 25, NOW(), NOW()),
+('Compass', 25, NOW(), NOW()),
+('Wrangler', 25, NOW(), NOW()),
+
+-- Dacia (26)
+('Sandero', 26, NOW(), NOW()),
+('Duster', 26, NOW(), NOW()),
+('Jogger', 26, NOW(), NOW()),
+
+-- Mitsubishi (27)
+('ASX', 27, NOW(), NOW()),
+('Outlander', 27, NOW(), NOW()),
+('L200', 27, NOW(), NOW()),
+
+-- Alfa Romeo (28)
+('Giulia', 28, NOW(), NOW()),
+('Stelvio', 28, NOW(), NOW()),
+('Tonale', 28, NOW(), NOW()),
+
+-- Suzuki (29)
+('Swift', 29, NOW(), NOW()),
+('Vitara', 29, NOW(), NOW()),
+('Jimny', 29, NOW(), NOW()),
+
+-- Mini (30)
+('Cooper', 30, NOW(), NOW()),
+('Countryman', 30, NOW(), NOW()),
+('Clubman', 30, NOW(), NOW());
+
+
+
+-- admin@admin.com admin1234
+
+-- test@test.com test1234
+
+-- david@gmail.com david1234
